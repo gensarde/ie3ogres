c5841c822f7ba172880c954481b4a6be46450f02
gensarde <72407407+gensarde@users.noreply.github.com>
2026-07-09 19:16:50 +0200

    Remove makefiles

--- filesystem.mk
+++
@@ -1,8 +0,0 @@
-include graphics_files_rules.mk
-
-NITROFS_FILES_FILE := nitrofs_files.txt
-
-NITROFS_FILES := $(tail -c +15 ${NITROFS_FILES_FILE})
-
-.PHONY: filesystem
-filesystem: $(NITROFS_FILES)
